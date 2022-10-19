c9ca8bdf8c77495786f40cc1e159bc439141748f
yunha <wjh0970@gmail.com>
2022-10-19 19:34:53 +0900

    first commit

---
+++ assets/res/www/js/mcore.extends.js
@@ -0,0 +1,26 @@
+
+
+/**
+* 공통 라이브러리는 importFiles 배열에 선언한다.
+* 선언된 라이브러리들은 html 파일에서
+* <script src="../js/mcore.min.js"></script>
+* 가 추가된곳 자동으로 임포트된다.
+*/
+(function(window, undefined) {
+
+var 
+thisFileName = "mcore.extends.js",
+
+importFiles = [
+	"jquery-3.6.0.min.js",
+	"muikit-1.0.0.min.js",
+	"wnInterface.extends.js",
+  "common/ui.js",
+  "common/definition.js",
+  "common/jquery.plugin.js",
+  "common/setup.js",
+];
+
+M.ScriptLoader.writeScript( importFiles, M.ScriptLoader.scriptPath(thisFileName) );
+
+})(window);
